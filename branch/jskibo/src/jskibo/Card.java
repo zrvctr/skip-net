d6d8927b366de1bab8e304823bd830344289cd17
Uplink.Coder <Uplink.Coder@07b787e8-bcf2-11de-9616-7b1d4728551e>
2012-10-28 16:31:35 +0000

    added toString method to Cards class

--- branch/jskibo/src/jskibo/Card.java
+++ branch/jskibo/src/jskibo/Card.java
@@ -1,24 +1,32 @@
 package jskibo;
 
-
 public class Card {
-	private int cardValue;
-	
-        protected Card(int cardval) {
-		if (cardval > 1 || cardval < 14) {
-			cardValue = cardval;
-		} else {
-                    System.out.println("You cannot stick an illigal Card in here");
-                }
-                    
-	}
-	
-	public int getValue() {
-		return cardValue;
-	}
-      
-        
-        boolean isJoker() {
-            return cardValue==13;
+    private int cardValue;
+
+    protected Card(int cardval) {
+        if ((cardval > 1) || (cardval < 14)) {
+            cardValue = cardval;
+        } else {
+            System.out.println("You cannot stick an illigal Card in here");
         }
+    }
+
+    public int getValue() {
+        return cardValue;
+    }
+
+    @Override
+    public String toString() {
+        if (this.getValue() == 13) {
+            return ("J");
+        } else {
+            return new Integer(this.getValue()).toString();
+
+            // crates a new Interger with the Value as argument
+            // and Converts this into a String witch in turn is returned
+        }
+    }
 }
+
+
+//~ Formatted by Jindent --- http://www.jindent.com
